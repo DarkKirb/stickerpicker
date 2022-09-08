80d9f6d5df7b473a78f73239b92eb0c3874d0198
Charlotte 🦝 Delenk <lotte@chir.rs>
2022-09-08 08:14:08 +0100

    update sticker picker

--- build/lib/sticker/version.py
+++ build/lib/sticker/version.py
@@ -1,6 +1,6 @@
 # Generated in setup.py
 
 git_tag = None
-git_revision = '99ced887'
-version = '0.1.0+dev.99ced887'
-linkified_version = '0.1.0+dev.[99ced887](https://github.com/maunium/stickerpicker/commit/99ced8878aaf89a47dd8bdb592025ae2160d6f07)'
+git_revision = 'fe647d6b'
+version = '0.1.0+dev.fe647d6b'
+linkified_version = '0.1.0+dev.[fe647d6b](https://github.com/maunium/stickerpicker/commit/fe647d6bb036f9f4c9c2280f45d04769d97515a0)'
