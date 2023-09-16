e329dda53051f0e8732e556dc25735c9e183c8ad
Charlotte 🦝 Delenk <lotte@chir.rs>
2023-09-16 12:56:01 +0100

    update stickers

--- sticker/version.py
+++ sticker/version.py
@@ -1,6 +1,6 @@
 # Generated in setup.py
 
 git_tag = None
-git_revision = '1960a323'
-version = '0.1.0+dev.1960a323'
-linkified_version = '0.1.0+dev.[1960a323](https://github.com/maunium/stickerpicker/commit/1960a3233dd37fcea11440719885b606dad7d987)'
+git_revision = '080424c2'
+version = '0.1.0+dev.080424c2'
+linkified_version = '0.1.0+dev.[080424c2](https://github.com/maunium/stickerpicker/commit/080424c23edf1f345b132e77362fbac902a06591)'
---
+++ update.sh
@@ -0,0 +1,1 @@
+(sticker-import --list | awk '{print $(NF)}' | sed 's/^.//' | sed 's/.$//' | egrep -v '^acks$'; jq -r '.packs[]' web/packs/index.json | sed 's/\.json$//' | sed 's|^|t.me/addstickers/|'; for f in $@; do echo $f; done) | sort -u | xargs sticker-import
