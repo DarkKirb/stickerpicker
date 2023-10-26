37707576c594242332bdad39252d40ca7958c436
Charlotte 🦝 Delenk <lotte@chir.rs>
2023-10-26 08:18:20 +0100

    update

--- build/lib/sticker/version.py
+++ build/lib/sticker/version.py
@@ -1,6 +1,6 @@
 # Generated in setup.py
 
 git_tag = None
-git_revision = 'dc9e5bd4'
-version = '0.1.0+dev.dc9e5bd4'
-linkified_version = '0.1.0+dev.[dc9e5bd4](https://github.com/maunium/stickerpicker/commit/dc9e5bd43e239cc7ab645bd246fa99a98da23b3c)'
+git_revision = '59b5d53c'
+version = '0.1.0+dev.59b5d53c'
+linkified_version = '0.1.0+dev.[59b5d53c](https://github.com/maunium/stickerpicker/commit/59b5d53c38b800afbe2a78027818e2fb2d43da68)'
