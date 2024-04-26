9b5c86217ecca728cc246ae628dca1617416c5de
Charlotte 🦝 Delenk <lotte@chir.rs>
2024-04-26 18:17:50 +0100

    update

--- sticker/version.py
+++ sticker/version.py
@@ -1,6 +1,6 @@
 # Generated in setup.py
 
 git_tag = None
-git_revision = '635f3fbb'
-version = '0.1.0+dev.635f3fbb'
-linkified_version = '0.1.0+dev.[635f3fbb](https://github.com/maunium/stickerpicker/commit/635f3fbb4b064e300e3585a1522b911bde11ce16)'
+git_revision = 'ce549501'
+version = '0.1.0+dev.ce549501'
+linkified_version = '0.1.0+dev.[ce549501](https://github.com/maunium/stickerpicker/commit/ce54950178d91a7e16eb551046315e22ebbd920d)'
